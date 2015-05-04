d933b7da38d956ed47b6bc7fda2c0605b5e3c0db
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-05-04 13:51:53 -0700

    add the bit shift in EME algo

--- EME.py
+++ EME.py
@@ -28,8 +28,15 @@ def EME_E(T,K,P):
 
     # TO DO : L needs to time 2 later !
     L =  cipher.encrypt(IV)
-    #print "L before shift is ", L
-    #L = shift_left(L, 1)
+    a = len(L)
+    print "1 ", a
+    L = shift_left(L, 1)
+    b = len(L)
+    print "2 ", b
+
+    if a != b:
+        print "length mismatch"
+
     print "L is ", L
 
     count = 0
@@ -39,7 +46,7 @@ def EME_E(T,K,P):
         count = count + 1
         # TO DO version : PP = strxor_c(P_i, pow(2,i-1) * L)
         print "P_i is ", P_i
-        PP = strxor.strxor(P_i,L)
+        PP = strxor.strxor(P_i,shift_left(L,i-1))
         PPP = cipher.encrypt(PP)
         PPP_list.append(PPP)
 
@@ -58,7 +65,7 @@ def EME_E(T,K,P):
     CCC_list = [" "]* m
     for i in range(1,len(PPP_list)):
         #CCC_list[i] = strxor.strxor(PPP_list[i],pow(2,i-1)*M)
-        CCC_list[i] = strxor.strxor(PPP_list[i], M)
+        CCC_list[i] = strxor.strxor(PPP_list[i], shift_left(M, i))
 
     SC = " " * 16
     for i in range(1,len(CCC_list)):
@@ -70,7 +77,7 @@ def EME_E(T,K,P):
     for i in range(0,len(CCC_list)):
         CC = cipher.encrypt(CCC_list[i])
         #C = strxor(CC,pow(2, i) * L)
-        C = strxor.strxor(CC, L)
+        C = strxor.strxor(CC, shift_left(L,i))
         C_list.append(C)
 
 
@@ -100,6 +107,7 @@ def EME_D(T,K,C):
 
     # TO DO : L needs to time 2 later !
     L =  cipher.encrypt(IV)
+    L = shift_left(L, 1)
     print "L is ", L
 
     count = 0
@@ -109,7 +117,7 @@ def EME_D(T,K,C):
         count = count + 1
         # TO DO version : PP = strxor_c(P_i, pow(2,i-1) * L)
         print "C_i is ", C_i
-        CC = strxor.strxor(C_i,L)
+        CC = strxor.strxor(C_i,shift_left(L,i-1))
         CCC = cipher.decrypt(CC)
         CCC_list.append(CCC)
 
@@ -127,7 +135,7 @@ def EME_D(T,K,C):
 
     PPP_list = [" "] * m # store all the PPP obtained
     for i in range(1,len(CCC_list)):
-        PPP = strxor.strxor(CCC_list[i],M)
+        PPP = strxor.strxor(CCC_list[i],shift_left(M,i))
         PPP_list[i] = PPP
 
     SP = " " * n
@@ -139,7 +147,7 @@ def EME_D(T,K,C):
     P_list = []
     for i in range (0,len(PPP_list)):
         PP = cipher.decrypt(PPP_list[i])
-        P = strxor.strxor(PP,L)
+        P = strxor.strxor(PP,shift_left(L,i))
         P_list.append(P)
 
     plaintext = " "
@@ -160,31 +168,49 @@ T = os.urandom(16)
 K = os.urandom(16)
 P = os.urandom(32)
 
+print T
+print K
+print P
 def shift_left(input_string,num_of_bits):
+    bytes_tmp = []
+    for x in input_string:
+        bytes_tmp.append(ord(x))
+    print bytes_tmp
     # convert input string to binary string
     binary_str = ''.join('{0:08b}'.format(ord(x), 'b') for x in input_string)
-    print int(binary_str,2)
-    print int(binary_str,2) << num_of_bits
+    # print int(binary_str,2)
+    # print int(binary_str,2) << num_of_bits
     # shift and convert back to unicode string
+    # print int(binary_str, 2) << num_of_bits
+    a = len(binary_str)
+    print "shift left by ", num_of_bits
+    print binary_str
+    print len(binary_str)
     binary_str = "{0:b}".format(int(binary_str, 2) << num_of_bits)
-    # fill in 0s in front
-    while len(binary_str) % 8 != 0:
-        binary_str = '0' + binary_str
+    print binary_str
+    b = len(binary_str)
+    print len(binary_str)
+    # shift out bits, python doesn't handle this
+    if a < b:
+        binary_str = binary_str[num_of_bits:]
+    # if old string is longer than the current one, 0s are ignored by python
+    elif a > b:
+        while(len(binary_str) % 8 != 0):
+            binary_str = '0' + binary_str
+    
     # convert back to unicode string
     bytes_lst = []
     i = 0
     while i < len(binary_str):
-        print binary_str[i:i+8]
+        # print binary_str[i:i+8]
         bytes_lst.append(int(binary_str[i:i+8],2))
         i = i + 8
+    print bytes_lst
     new_str = ''.join(map(chr,bytes_lst))
     return new_str
 
-st = "4321"
-print (shift_left(st, 1))
-
-print "length of the plaintext ",len("A really secret message. Not for prying eyes.  \0\0\n hellohellohel")
-cipher_text = EME_E(T,K,"A really secret message. Not for prying eyes.  \0\0\n hellohellohel")
+print "length of the plaintext ",len("A really secret message. Not for prying eyes.   ")
+cipher_text = EME_E(T,K,"A really secret message. Not for prying eyes.   ")
 print "cipher_text is ", cipher_text
 print "length of cipher_text is ", len(cipher_text)
 decipered_text = EME_D(T,K,cipher_text)
